045f073ba9e230338ca5e0638baac585238625da
Henrique Barros <henriquebarrosx@gmail.com>
2024-03-28 11:52:13 +0000

    Fix nginx port number

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 # Read about: https://medium.com/@techrally/phoenix-setting-up-env-variables-6557eb1370ee
-# echo "Loading env variables..."
 source .env
 
-# envsubst '${PORT} ${DOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-# sed -e "s/\${PORT}/$PORT/g" -e "s/\${DOMAIN}/$DOMAIN/g" /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Start Nginx
-# echo "Starting NGINX..."
-# exec nginx -g 'daemon off;'
-
-# echo "Starting server..."
 /app/bin/server
 
